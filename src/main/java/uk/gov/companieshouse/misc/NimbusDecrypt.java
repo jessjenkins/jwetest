3d016f1d3a381ac40d1d3300d34b41116b60c168
Jessica Jenkins <jjenkins1@companieshouse.gov.uk>
2016-02-22 14:26:42 +0000

    Tidy up a bit

--- src/main/java/uk/gov/companieshouse/misc/NimbusDecrypt.java
+++ src/main/java/uk/gov/companieshouse/misc/NimbusDecrypt.java
@@ -2,17 +2,13 @@ package uk.gov.companieshouse.misc;
 
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.DirectDecrypter;
-import org.jose4j.jwe.ContentEncryptionAlgorithmIdentifiers;
-import org.jose4j.jwe.JsonWebEncryption;
-import org.jose4j.jwe.KeyManagementAlgorithmIdentifiers;
-import org.jose4j.keys.AesKey;
 
 import java.security.Key;
 
 /**
  * Created by jjenkins1 on 19/02/16.
  */
-public class JoseDecrypt {
+public class NimbusDecrypt {
 
 
     public static void main(String[] args) {
@@ -24,12 +20,13 @@ public class JoseDecrypt {
             String serializedJwe = "eyJhbGciOiJkaXIiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0..8xFJdER78EUOXfyCW995JA.HilGHhHcSUP0rrVNK3bTRw.Z8QZAwrjMDlSaNB8DZRDpA";
 
 
-            //Decode using jose4j
-            Key key = new AesKey(keybytes);
-            JsonWebEncryption jwe = new JsonWebEncryption();
-            jwe.setKey(key);
-            jwe.setCompactSerialization(serializedJwe);
-            System.out.println("Payload: " + jwe.getPayload());
+            //decode using nimbus
+            JWEObject jweObject = JWEObject.parse(serializedJwe);
+            jweObject.decrypt(new DirectDecrypter(keybytes));
+            Payload payload = jweObject.getPayload();
+            System.out.print("Numbus Decrypted payload:");
+            System.out.println(payload.toString());
+
 
         } catch (Exception ex) {
             ex.printStackTrace();
